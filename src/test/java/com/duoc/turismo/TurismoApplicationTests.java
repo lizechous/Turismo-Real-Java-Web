fd03562e938675f67c0dce06f85b3eeb364fc20d
lizechous <lizziepastenes@gmail.com>
2022-08-29 11:52:12 -0400

    fuentes

---
+++ src/test/java/com/duoc/turismo/TurismoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.turismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
